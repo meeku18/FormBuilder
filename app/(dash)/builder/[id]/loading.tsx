6971cd7fcc76f4b1bd35c3f3043af51bb903193a
meeku18 <hrishabhjais18@gmail.com>
2024-06-08 12:15:04 +0530

    loading change

--- app/(dash)/builder/[id]/loading.tsx
+++ app/(dash)/builder/[id]/loading.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { ImSpinner2 } from "react-icons/im";
 
-export function loading(){
+function Loading(){
     return <div className="w-full h-full flex items-center justify-center">
         <ImSpinner2 className="animate-spin"></ImSpinner2>
     </div>
 }
+export default Loading;
